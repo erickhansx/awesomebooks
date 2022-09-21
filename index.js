d0c3e2c66b991a6e2823f6f59d1f26a381d0d64e
erickhansx <erickhansx@gmail.com>
2022-09-21 16:51:00 -0500

    Refactor functions to classes

--- index.js
+++ index.js
@@ -5,76 +5,89 @@ const booksDiv = document.querySelector('.booksDiv');
 
 let books = [];
 
-const renderBooks = () => {
-  booksDiv.innerHTML = '';
-  books.forEach((book) => {
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
-    bookDiv.innerHTML = `
-      <h3>${book.title}</h3>
-      <p>${book.author}</p>
-      <button class="remove-btn">Remove</button>
-    `;
-    booksDiv.appendChild(bookDiv);
-    localStorage.setItem('books', JSON.stringify(books));
-  });
-};
+// Event Listeners
 
 window.addEventListener('DOMContentLoaded', () => {
   const storageBooks = JSON.parse(localStorage.getItem('books'));
   if (storageBooks) {
     books = storageBooks;
-    renderBooks();
+    UI.renderBooks();
   }
 });
-
-const validateForm = () => {
-  if (inputTitle.value === '' || inputAuthor.value === '') {
-    const alertDiv = document.createElement('div');
-    alertDiv.classList.add('alert');
-    alertDiv.innerHTML = 'Please fill in all fields';
-    document.body.appendChild(alertDiv);
-    setTimeout(() => {
-      alertDiv.remove();
-    }, 2000);
-    return false;
-  }
-  return true;
-};
-
-const addBook = (e) => {
-  e.preventDefault();
-  if (validateForm()) {
-    const title = inputTitle.value;
-    const author = inputAuthor.value;
-    const book = {
-      title,
-      author,
-    };
-    books.push(book);
-    localStorage.setItem('books', JSON.stringify(books));
-    inputAuthor.value = '';
-    inputTitle.value = '';
-  }
-};
 
 addBtn.addEventListener('click', (e) => {
-  addBook(e);
-  renderBooks();
+  UI.addBook(e);
+  UI.renderBooks();
 });
-
-const removeFromStorage = (book) => {
-  const bookTitle = book.querySelector('h3').textContent;
-  const bookAuthor = book.querySelector('p').textContent;
-  books = books.filter(
-    (book) => book.title !== bookTitle && book.author !== bookAuthor,
-  );
-  localStorage.setItem('books', JSON.stringify(books));
-};
 
 booksDiv.addEventListener('click', (e) => {
   if (e.target.classList.contains('remove-btn')) {
     e.target.parentElement.remove();
-    removeFromStorage(e.target.parentElement);
+    Del.removeFromStorage(e.target.parentElement);
   }
 });
+
+// Class: UI -- To handle UI tasks
+class UI {
+  static addBook(e) {
+    e.preventDefault();
+    if (this.validateForm()) {
+      const title = inputTitle.value;
+      const author = inputAuthor.value;
+      const book = {
+        title,
+        author,
+      };
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+      inputAuthor.value = '';
+      inputTitle.value = '';
+    }
+  }
+
+  static renderBooks() {
+    booksDiv.innerHTML = '';
+    books.forEach((book) => {
+      const bookDiv = document.createElement('div');
+      bookDiv.classList.add('book');
+      bookDiv.innerHTML = `
+        <p id= "Ptitle">"${book.title}" by <span id="Pauthor">${book.author}</span></p>
+        <button class="remove-btn">Remove</button>
+      `;
+      booksDiv.appendChild(bookDiv);
+      localStorage.setItem('books', JSON.stringify(books));
+      if (booksDiv !== null) {
+        booksDiv.classList.add('border');
+      }
+    });
+  }
+
+  static validateForm() {
+    if (inputTitle.value === '' || inputAuthor.value === '') {
+      const alertDiv = document.createElement('div');
+      alertDiv.classList.add('alert');
+      alertDiv.innerHTML = 'Please fill in all fields';
+      document.body.appendChild(alertDiv);
+      setTimeout(() => {
+        alertDiv.remove();
+      }, 2000);
+      return false;
+    }
+    return true;
+  }
+}
+
+// Class: Del -- To delete books from the local storage
+class Del {
+  static removeFromStorage(book) {
+    const bookTitle = book.querySelector('#Ptitle').textContent;
+    const bookAuthor = book.querySelector('#Pauthor').textContent;
+    books = books.filter(
+      (book) => book.title !== bookTitle && book.author !== bookAuthor
+    );
+    localStorage.setItem('books', JSON.stringify(books));
+    if (booksDiv.innerHTML === '') {
+      booksDiv.classList.remove('border');
+    }
+  }
+}
