1eedc02bbcde77828b3e3451b584f73658ca971e
erickhansx <erickhansx@gmail.com>
2022-09-21 09:24:38 -0500

    Add functions for add book, remove book from ui and from localStorage

---
+++ index.js
@@ -0,0 +1,81 @@
+const inputTitle = document.querySelector('#title');
+const inputAuthor = document.querySelector('#author');
+const addBtn = document.querySelector('.add-btn');
+const booksDiv = document.querySelector('.booksDiv');
+const removeBtn = document.querySelectorAll('.remove-btn');
+
+let books = [];
+
+window.addEventListener('DOMContentLoaded', () => {
+  const storageBooks = JSON.parse(localStorage.getItem('books'));
+  if (storageBooks) {
+    books = storageBooks;
+    renderBooks();
+  }
+});
+
+const addBook = (e) => {
+  e.preventDefault();
+  if (validateForm()) {
+    const title = inputTitle.value;
+    const author = inputAuthor.value;
+    const book = {
+      title,
+      author,
+    };
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+    inputAuthor.value = '';
+    inputTitle.value = '';
+  }
+};
+
+const renderBooks = () => {
+  booksDiv.innerHTML = '';
+  books.forEach((book) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+    bookDiv.innerHTML = `
+      <h3>${book.title}</h3>
+      <p>${book.author}</p>
+      <button class="remove-btn">Remove</button>
+    `;
+    booksDiv.appendChild(bookDiv);
+    localStorage.setItem('books', JSON.stringify(books));
+  });
+};
+
+addBtn.addEventListener('click', (e) => {
+  addBook(e);
+  renderBooks();
+});
+
+const removeFromStorage = (book) => {
+  const bookTitle = book.querySelector('h3').textContent;
+  const bookAuthor = book.querySelector('p').textContent;
+  books = books.filter(
+    (book) => book.title !== bookTitle && book.author !== bookAuthor
+  );
+  localStorage.setItem('books', JSON.stringify(books));
+};
+
+const validateForm = () => {
+  if (inputTitle.value === '' || inputAuthor.value === '') {
+    const alertDiv = document.createElement('div');
+    alertDiv.classList.add('alert');
+    alertDiv.innerHTML = `Please fill in all fields`;
+    document.body.appendChild(alertDiv);
+    setTimeout(() => {
+      alertDiv.remove();
+    }, 2000);
+    return false;
+  }
+  return true;
+};
+
+booksDiv.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove-btn')) {
+    e.target.parentElement.remove();
+    removeFromStorage(e.target.parentElement);
+  }
+});
