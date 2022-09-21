fa9a50b8df5e0eaac3a115880399d9ba84e0af6b
erickhansx <erickhansx@gmail.com>
2022-09-21 10:13:43 -0500

    Fix linter errors

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# todolist
-Todo List 
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Todo List
+
+> This project was created with the intention of storing To-do activities in a list and having the capability of adding and removing items to the list and also preserving them in localStorage.
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+
+## Live Demo (if available)
+
+[Live Demo Link](https://livedemo.com)
+
+## Getting Started
+
+**This is an example of how you may give instructions on setting up your project locally.**
+**Modify this file to match your project, remove sections that don't apply. For example: delete the testing section if the currect project doesn't require testing.**
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+### Setup
+
+### Install
+
+### Usage
+
+### Run tests
+
+### Deployment
+
+## Authors
+
+👤 **Author1**
+
+- GitHub: [@githubhandle](https://github.com/githubhandle)
+- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+
+👤 **Kalkidan Daniel**
+
+- GitHub: [@kal2944](https://github.com/kal2944)
+- Twitter: [@Kalkida01088197](https://twitter.com/Kalkida01088197)
+- LinkedIn: [@kalkidan Daniel](https://www.linkedin.com/in/kalkidan-daniel-b2a204238/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const inputTitle = document.querySelector('#title');
 const inputAuthor = document.querySelector('#author');
 const addBtn = document.querySelector('.add-btn');
 const booksDiv = document.querySelector('.booksDiv');
-const removeBtn = document.querySelectorAll('.remove-btn');
 
 let books = [];
+
+const renderBooks = () => {
+  booksDiv.innerHTML = '';
+  books.forEach((book) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+    bookDiv.innerHTML = `
+      <h3>${book.title}</h3>
+      <p>${book.author}</p>
+      <button class="remove-btn">Remove</button>
+    `;
+    booksDiv.appendChild(bookDiv);
+    localStorage.setItem('books', JSON.stringify(books));
+  });
+};
 
 window.addEventListener('DOMContentLoaded', () => {
   const storageBooks = JSON.parse(localStorage.getItem('books'));
@@ -13,6 +27,20 @@ window.addEventListener('DOMContentLoaded', () => {
     renderBooks();
   }
 });
+
+const validateForm = () => {
+  if (inputTitle.value === '' || inputAuthor.value === '') {
+    const alertDiv = document.createElement('div');
+    alertDiv.classList.add('alert');
+    alertDiv.innerHTML = 'Please fill in all fields';
+    document.body.appendChild(alertDiv);
+    setTimeout(() => {
+      alertDiv.remove();
+    }, 2000);
+    return false;
+  }
+  return true;
+};
 
 const addBook = (e) => {
   e.preventDefault();
@@ -30,21 +58,6 @@ const addBook = (e) => {
   }
 };
 
-const renderBooks = () => {
-  booksDiv.innerHTML = '';
-  books.forEach((book) => {
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
-    bookDiv.innerHTML = `
-      <h3>${book.title}</h3>
-      <p>${book.author}</p>
-      <button class="remove-btn">Remove</button>
-    `;
-    booksDiv.appendChild(bookDiv);
-    localStorage.setItem('books', JSON.stringify(books));
-  });
-};
-
 addBtn.addEventListener('click', (e) => {
   addBook(e);
   renderBooks();
@@ -54,23 +67,9 @@ const removeFromStorage = (book) => {
   const bookTitle = book.querySelector('h3').textContent;
   const bookAuthor = book.querySelector('p').textContent;
   books = books.filter(
-    (book) => book.title !== bookTitle && book.author !== bookAuthor
+    (book) => book.title !== bookTitle && book.author !== bookAuthor,
   );
   localStorage.setItem('books', JSON.stringify(books));
-};
-
-const validateForm = () => {
-  if (inputTitle.value === '' || inputAuthor.value === '') {
-    const alertDiv = document.createElement('div');
-    alertDiv.classList.add('alert');
-    alertDiv.innerHTML = `Please fill in all fields`;
-    document.body.appendChild(alertDiv);
-    setTimeout(() => {
-      alertDiv.remove();
-    }, 2000);
-    return false;
-  }
-  return true;
 };
 
 booksDiv.addEventListener('click', (e) => {
