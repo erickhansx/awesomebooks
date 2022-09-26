fa498268c21937e9bd1bb0aec3f5f81b36a9582d
erickhansx <erickhansx@gmail.com>
2022-09-26 14:18:11 -0500

    Format file tree into modules, rename css file to index.css, Use luxon library to add date to UI

--- src/modules/DelClass.js
+++ src/modules/DelClass.js
@@ -1,8 +1,8 @@
-let books = JSON.parse(localStorage.getItem('books'));
 const booksDiv = document.querySelector('.booksDiv');
 
 export default class Del {
   static removeFromStorage(book) {
+    let books = JSON.parse(localStorage.getItem('books'));
     const bookTitle = book.querySelector('#Ptitle').textContent;
     const bookAuthor = book.querySelector('#Pauthor').textContent;
     books = books.filter(
